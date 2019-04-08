4b2e62ab0f937f7b9239eb46a7611a80a505bdde
Gianfranco Polinesi <gianfrancopolinesi@outlook.com>
2019-04-07 21:54:34 -0300

    Modulo input implementado

---
+++ input.py
@@ -0,0 +1,185 @@
+'''
+    Modulo: InputHandler
+
+    Interface
+    ---------
+        update()
+            Actualiza el estado del input
+        get(ACCION)
+            Obtiene si el botón para realizar ACCION está apretado
+        getDevicesList()
+            Muestra los dispositivos (Joystick) disponibles
+        getDevice()
+            Obtiene el id del dispositivo utilizado por el input
+        setDevice(deviceName)
+            Marca como predeterminado el dispositivo seleccionado
+
+        @TODO: Implementar:
+        getConfig(device=currentDevice)
+            Obtiene un diccionario con la configuración del control
+        setConfig(dict, device=currentDevice)
+            Actualiza el diccionario con la configuración en dict
+
+    Uso:
+    ----
+
+        player.input = InputHandler()
+
+        ...
+        player.input.update()
+
+        if player.input.get("jump"):
+            player.y -= player.jumpspeed
+        ...
+
+'''
+
+import pygame
+from enum import Enum
+
+class Device():
+    def __init__(self, name="", type=""):
+        self.name = name
+        self.type = type
+        self.configuration = {}
+
+class InputHandler():
+    def __init__(self):
+        self.inGameControls = {
+            "left"                  :   0,
+            "right"                 :   0,
+            "jump"                  :   0,
+            "pause"                 :   0,
+            "drop"                  :   0,
+            "reset"                 :   0,
+            "escape"                :   0,
+            "chat"                  :   0,
+            "slot1"                 :   0,
+            "slot2"                 :   0,
+            "slot3"                 :   0,
+            "slot4"                 :   0,
+            "prevSlot"              :   0,
+            "nextSlot"              :   0,
+            "primary"               :   0,
+            "secondary"             :   0,
+
+            #Pointer
+            "pointer"              :   (0,0)
+        }
+
+        #Init Mouse
+        self.keyboardAndMouse = MouseAndKeyboard()
+        self.currentDevice = self.keyboardAndMouse
+
+        #Joysticks
+        if pygame.joystick.get_init() != True:
+            #Inicializar Joysticks
+            pygame.joystick.init()
+            
+        #Obtener la lista de joysticks disponibles
+        self.joysticks = []
+        for x in range(pygame.joystick.get_count()):
+            joystick = pygame.joystick.Joystick(x)
+            self.joysticks.append(Joystick(name=joystick.get_name(), joystick=joystick))
+
+        self.devices = [self.keyboardAndMouse] + self.joysticks
+
+    def get(self, accion):
+        return self.inGameControls[accion]
+
+    def getDevicesList(self):
+        return self.devices
+
+    def setDevice(self, device):
+        l = list(filter(lambda x: x.name == device, self.devices))
+        if len(l) == 1:
+            self.currentDevice = l[0]
+            if self.currentDevice.type == "Joystick":
+                self.currentDevice.joystick.init()
+
+    def getDevice(self):
+        return self.currentDevice
+
+    def update(self):
+
+        if self.currentDevice.type == "MouseAndKeyboard":
+            mouse = pygame.mouse.get_pressed()
+            keyboard = pygame.key.get_pressed()
+            for k, v in self.currentDevice.configuration.items():
+                if v:
+                    device = v[0]
+                    if device == 'keyboard':
+                        self.inGameControls[k] = keyboard[v[1]]
+                    elif device == 'mouse':
+                        self.inGameControls[k] = mouse[v[1].value]
+            self.inGameControls["pointer"] = pygame.mouse.get_pos()
+
+        elif self.currentDevice.type == "Joystick":
+            for k, v in self.currentDevice.configuration.items():
+                if v:
+                    component = v[0]
+                    if component == 'axis':
+                        self.inGameControls[k] = (abs(v[2] - self.currentDevice.joystick.get_axis(v[1])) < 0.1)
+                    elif component == 'button':
+                        self.inGameControls[k] = self.currentDevice.joystick.get_button(v[1])
+                    elif component == 'hat':
+                        self.inGameControls[k] = (self.currentDevice.joystick.get_hat(v[1]) == v[2])
+            self.inGameControls["pointer"] = (self.currentDevice.joystick.get_axis(3), self.currentDevice.joystick.get_axis(4))
+
+
+class MOUSEBUTTON(Enum):
+    LEFTBUTTON = 0
+    MIDDLEBUTTON = 1
+    RIGHTBUTTON = 2
+
+class MouseAndKeyboard(Device):
+    '''
+        clase 
+    '''
+    def __init__(self):
+        self.name = "Mouse and Keyboard"
+        self.type = "MouseAndKeyboard"
+        self.defaultConfiguration = {
+            "left"              :   ('keyboard',pygame.K_a),
+            "right"             :   ('keyboard',pygame.K_d),
+            "jump"              :   ('keyboard',pygame.K_SPACE),
+            "pause"             :   ('keyboard',pygame.K_RETURN),
+            "drop"              :   ('keyboard',pygame.K_g),
+            "reset"             :   ('keyboard',pygame.K_r),
+            "escape"            :   ('keyboard',pygame.K_ESCAPE),
+            "chat"              :   ('keyboard',pygame.K_t),
+            "slot1"             :   ('keyboard',pygame.K_1),
+            "slot2"             :   ('keyboard',pygame.K_2),
+            "slot3"             :   ('keyboard',pygame.K_3),
+            "slot4"             :   ('keyboard',pygame.K_4),
+            "prevSlot"          :   ('keyboard',pygame.K_q),
+            "nextSlot"          :   ('keyboard',pygame.K_e),
+            "primary"           :   ('mouse',MOUSEBUTTON.LEFTBUTTON),
+            "secondary"         :   ('mouse',MOUSEBUTTON.RIGHTBUTTON)
+        }
+        self.configuration = self.defaultConfiguration
+
+class Joystick(Device):
+    def __init__(self, name, joystick):
+        self.name = name
+        self.type = "Joystick"
+        self.joystick = joystick
+        self.defaultConfiguration = {
+            "left"              :   ('axis',0,-1),
+            "right"             :   ('axis',0, 1),
+            "jump"              :   ('axis',1,-1),
+            "pause"             :   ('button',7),
+            "drop"              :   ('button',3),
+            "reset"             :   None,
+            "escape"            :   ('button',1),
+            "chat"              :   ('button',6),
+            "slot1"             :   None,
+            "slot2"             :   None,
+            "slot3"             :   None,
+            "slot4"             :   None,
+            "prevSlot"          :   ('hat',0, (-1,0)),
+            "nextSlot"          :   ('hat',0, ( 1,0)),
+            "primary"           :   ('axis',2, -1),
+            "secondary"         :   None
+        }
+        self.configuration = self.defaultConfiguration
---
+++ inputTest.py
@@ -0,0 +1,150 @@
+import pygame
+from input import InputHandler
+import pprint
+
+from itertools import chain
+
+def truncline(text, font, maxwidth):
+        real=len(text)       
+        stext=text           
+        l=font.size(text)[0]
+        cut=0
+        a=0                  
+        done=1
+        old = None
+        while l > maxwidth:
+            a=a+1
+            n=text.rsplit(None, a)[0]
+            if stext == n:
+                cut += 1
+                stext= n[:-cut]
+            else:
+                stext = n
+            l=font.size(stext)[0]
+            real=len(stext)               
+            done=0                        
+        return real, done, stext             
+        
+def wrapline(text, font, maxwidth): 
+    done=0                      
+    wrapped=[]                  
+                               
+    while not done:             
+        nl, done, stext=truncline(text, font, maxwidth) 
+        wrapped.append(stext.strip())                  
+        text=text[nl:]                                 
+    return wrapped
+
+
+def wrap_multi_line(text, font, maxwidth):
+    """ returns text taking new lines into account.
+    """
+    lines = chain(*(wrapline(line, font, maxwidth) for line in text.splitlines()))
+    return list(lines)
+
+
+
+
+
+# Define some colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+
+# This is a simple class that will help us print to the screen
+# It has nothing to do with the joysticks, just outputting the
+# information.
+class TextPrint:
+    def __init__(self):
+        self.reset()
+        self.font = pygame.font.Font(None, 20)
+
+    def print(self, screen, textString):
+        textBitmap = self.font.render(textString, True, BLACK)
+        screen.blit(textBitmap, [self.x, self.y])
+        self.y += self.line_height
+        
+    def reset(self):
+        self.x = 10
+        self.y = 10
+        self.line_height = 15
+        
+    def indent(self):
+        self.x += 10
+        
+    def unindent(self):
+        self.x -= 10
+    
+
+pygame.init()
+ 
+# Set the width and height of the screen [width,height]
+size = [500, 700]
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("My Game")
+
+#Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# Initialize the joysticks
+pygame.joystick.init()
+    
+# Get ready to print
+textPrint = TextPrint()
+
+
+playerInput = InputHandler()
+
+
+# -------- Main Program Loop -----------
+while done==False:
+    # EVENT PROCESSING STEP
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done=True # Flag that we are done so we exit this loop
+        
+        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
+        if event.type == pygame.JOYBUTTONDOWN:
+            print("Joystick button pressed.")
+        if event.type == pygame.JOYBUTTONUP:
+            print("Joystick button released.")
+            
+    playerInput.update()
+
+    if playerInput.get("escape"):
+        if (playerInput.getDevice()).name == "Mouse and Keyboard":
+            if "Controller (Xbox One For Windows)" in map(lambda x: x.name, playerInput.getDevicesList()):
+                playerInput.setDevice("Controller (Xbox One For Windows)")
+        elif (playerInput.getDevice()).name == "Controller (Xbox One For Windows":
+            playerInput.setDevice("Mouse and Keyboard")
+
+    # DRAWING STEP
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    screen.fill(WHITE)
+    textPrint.reset()
+
+    for k,v in playerInput.inGameControls.items():
+        textPrint.print(screen, "{}, {}".format(k,v))
+
+    textPrint.print(screen, " ")
+
+    for k,v in playerInput.currentDevice.configuration.items():
+        textPrint.print(screen, "{}, {}".format(k,v))
+    #textPrint.indent()
+    
+    # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+    
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # Limit to 20 frames per second
+    clock.tick(20)
+    
+# Close the window and quit.
+# If you forget this line, the program will 'hang'
+# on exit if running from IDLE.
+pygame.quit ()
